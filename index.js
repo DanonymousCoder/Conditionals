b5ec1d8d8fc7c42ee94f82f60f9d805c951e62f4
Ridwan <aladebaruwa@gmail.com>
2023-06-10 18:48:37 +0100

    js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log( 2 > 1 );  // true (correct)
 console.log( 2 == 1 ); // false (wrong)
 console.log( 2 != 1 ); // true (correct)
+
+let result = 5 > 4; // assign the result of the comparison
+alert( result ); // true
