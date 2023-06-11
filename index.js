e835721866d4ee0088b299066f883b82412786a9
Ridwan <aladebaruwa@gmail.com>
2023-06-11 17:52:26 +0100

    JS

--- index.js
+++ index.js
@@ -104,3 +104,22 @@ if (x2 > 0) alert( 'Greater than zero!' );
 alert( !true );
 alert( !!null );
 alert(!!true);
+let userName = prompt("Who's there?", '');
+
+if (userName === 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass === 'TheMaster') {
+    alert( 'Welcome!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'Canceled' );
+  } else {
+    alert( 'Wrong password' );
+  }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}
