eacda60680c142cd7770e0d367cc9d48d188ac3d
Ridwan <aladebaruwa@gmail.com>
2023-06-11 18:17:35 +0100

    JS

--- index.js
+++ index.js
@@ -156,3 +156,12 @@ function setWeather() {
     para.textContent = "";
   }
 }
+
+let cheese = "Cheddar";
+
+if (cheese) {
+  console.log("Yay! Cheese available for making cheese on toast.");
+} else {
+  console.log("No cheese on toast for you today.");
+}
+
