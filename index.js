59c6eeec654d7859d553e9b3a6b00b82ab6eedb4
Ridwan <aladebaruwa@gmail.com>
2023-06-11 17:31:09 +0100

    JS

--- index.js
+++ index.js
@@ -78,3 +78,9 @@ let hour = 9;
 if (hour < 10 || hour > 18) {
   alert( 'The office is closed.' );
 }
+
+let firstName = "Ridwanullah";
+let lastName = "Baruwa";
+let nickName = "SuperCoder";
+
+alert( firstName || lastName || nickName || "Anonymous"); // SuperCoder
