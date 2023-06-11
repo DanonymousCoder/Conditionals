2f0ca0827c12b9aa1d107ca711fa99269654a89c
Ridwan <aladebaruwa@gmail.com>
2023-06-11 18:42:42 +0100

    JS

--- index.js
+++ index.js
@@ -178,3 +178,16 @@ if (iceCreamVanOutside || houseStatus === "on fire") {
 } else {
   console.log("Probably should just stay in then.");
 }
+
+const select = document.querySelector("select");
+const html = document.querySelector("html");
+document.body.style.padding = "10px";
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+select.addEventListener("change", () =>
+  select.value === "black" ? update("black", "white") : update("white", "black")
+);
